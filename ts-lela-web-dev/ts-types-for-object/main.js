3b4e4a6f8264aad67532bc7163576b407cad2ebe
Aditya Neve <aditya.n@tricog.com>
2022-05-24 11:02:23 +0530

    writing type in one line && with type keyword

--- ts-lela-web-dev/ts-types-for-object/main.js
+++ ts-lela-web-dev/ts-types-for-object/main.js
@@ -22,3 +22,15 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+var person1 = {
+    name: 'Leela',
+    age: 99,
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
+};
+person1.role.push('admin');
+console.log(person1);
+var employee = [5, 'asdfds'];
+var emp;
+emp = [22, 'adsfasd'];
+console.log(emp);
--- ts-lela-web-dev/ts-types-for-object/main.ts
+++ ts-lela-web-dev/ts-types-for-object/main.ts
@@ -12,7 +12,7 @@ console.log(person.name) */
 let person = {
     name: 'Leela',
     age: 99,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
 }
 
 let favorites1: any[]
@@ -28,3 +28,40 @@ favorites3 = [{ name: 'adsfs' }]
 for (let hobby of person.hobbies) {
     console.log(hobby.toUpperCase())
 }
+
+
+let person1: {
+    name: string,
+    age: number,
+    hobbies: string[],
+    role: [number, string]
+} = {
+    name: 'Leela',
+    age: 99,
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
+}
+
+person1.role.push('admin')
+console.log(person1)
+
+
+
+let employee: [
+    number,
+    string
+] = [5, 'asdfds']
+
+type Emp = [
+    number,
+    string
+]
+
+type Obj = {
+    name: string,
+    age: number,
+}
+
+let emp: Emp
+emp = [22, 'adsfasd']
+console.log(emp)
