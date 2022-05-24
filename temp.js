e19789ffdbfbdeb3236ba1033e2eb93927fcefdf
Aditya Neve <aditya.n@tricog.com>
2022-05-24 10:26:50 +0530

    practice ts

---
+++ temp.js
@@ -0,0 +1,6 @@
+console.clear();
+var array;
+array = ['aditya', 'gaurav'];
+var numberArray;
+numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("🚀 ~ file: temp.tsx ~ line 13 ~ numberArray", numberArray);
---
+++ temp.tsx
@@ -0,0 +1,14 @@
+console.clear();
+
+type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{name: string}>;
+
+
+
+let array: StringArray;
+array = ['aditya', 'gaurav']
+
+let numberArray: NumberArray;
+numberArray = [1,2,3,4,5,6,7,8,9,10]
+console.log("🚀 ~ file: temp.tsx ~ line 13 ~ numberArray", numberArray)
